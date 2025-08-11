585015838777b15a8472432705d2dccc0497f8f1
SyedAbdul05 <ksyedabdulwahab@gmail.com>
2025-08-11 22:49:52 +0530

    Add files via upload

---
+++ Basic Arithmetic/Arithmetic.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber;
+    int add, sub, mul;
+    int divisionResult;
+    int remainderValue;
+
+    cout << "Enter Two Numbers\n";
+    cout << "\nEnter First 1st Number : ";
+    
+
+    cin >> firstNumber;
+    cout << "\nEnter Second Number : ";
+    cin >> secondNumber;
+
+    //Main Calculations
+    add = firstNumber + secondNumber;       //Addition
+    sub = firstNumber - secondNumber;    //Subtraction
+    mul = firstNumber * secondNumber; //Multiplication
+    divisionResult = firstNumber / secondNumber;       //Division (Without Remainder)
+    remainderValue = firstNumber % secondNumber;       //Remainder value
+   
+    cout << "\n\nAddition Result = " << add;
+    cout << "\nSubtraction Result = " << sub;
+    cout << "\nMultiplication Result = " << mul;
+    cout << "\nDivision Result = "  << divisionResult;
+    cout << "\nRemainder value = " << remainderValue;
+    return 0;
+}
